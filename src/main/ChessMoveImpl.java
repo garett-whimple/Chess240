b4fd7192feb1bace4b6ddb4c04a644843e247188
garett_whimple <garett.whimple@gmail.com>
2023-10-02 21:36:59 -0600

    implemented chessMove

--- src/main/ChessMoveImpl.java
+++ src/main/ChessMoveImpl.java
@@ -3,18 +3,31 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class ChessMoveImpl implements ChessMove {
+
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+    private ChessPiece.PieceType promotionPiece = null;
+    public ChessMoveImpl(ChessPosition startPosition, ChessPosition endPosition) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+    }
+    public ChessMoveImpl(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
     @Override
     public ChessPosition getStartPosition() {
-        return null;
+        return startPosition;
     }
 
     @Override
     public ChessPosition getEndPosition() {
-        return null;
+        return endPosition;
     }
 
     @Override
     public ChessPiece.PieceType getPromotionPiece() {
-        return null;
+        return promotionPiece;
     }
 }
